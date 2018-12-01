86c3f3da068444220104fa41e5fd9b54836dbf5e
Michal Skora <skorek.michal@gmail.com>
2018-12-01 14:06:45 +0100

    Motor tests passed - servos stopped working?

--- airplane/airplane.ino
+++ airplane/airplane.ino
@@ -10,8 +10,10 @@ Servo servoMain2;
 Servo engine;
 int xMapped = -1;
 int yMapped = -1;
+int engineValue = 0;
 unsigned long last_usage_of_servos;
 boolean attached = false;
+int speed = 1218;
 
 void setup()
 {
@@ -33,6 +35,10 @@ void setup()
   delay(1000);
   servoMain2.detach();
   last_usage_of_servos = millis();
+
+  engine.attach(6);
+  delay(50);
+  arm(engine);
 }
 
 void loop()
@@ -41,6 +47,14 @@ void loop()
   {
     int text[3];
     radio.read(&text, sizeof(text));
+
+    if (engineValue != text[2])
+    {
+      engineValue = text[2];
+      engine.write(engineValue + speed);
+      Serial.print("engine: ");
+      Serial.println(engineValue + speed);
+    }
 
     if (xMapped == text[0] && yMapped == text[1])
     {
@@ -57,16 +71,16 @@ void loop()
     int normalizedX = min(max(xMapped, 10), 170);
     int normalizedY = min(max(yMapped, 10), 170);
 
-    if(87<=normalizedX || normalizedX<=93)
+    if (87 <= normalizedX || normalizedX <= 93)
     {
       normalizedX = 90;
     }
-    
-    if(87<=normalizedY || normalizedY<=93)
+
+    if (87 <= normalizedY || normalizedY <= 93)
     {
       normalizedY = 90;
     }
-    
+
     servoMain.attach(9);
     servoMain2.attach(10);
     attached = true;
@@ -81,4 +95,16 @@ void loop()
     servoMain.detach();
     servoMain2.detach();
   }
+}
+
+void arm(Servo m)
+{
+  Serial.print("Arming.");
+  m.write(0);
+  delay(100);
+  m.write(1224); //A value at which the motor starts turning
+  delay(2000);
+  m.write(1024); //A value at which the motor stands still
+  delay(3000);
+  Serial.println(" Armed!");
 }
--- controller/controller.ino
+++ controller/controller.ino
@@ -2,8 +2,8 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-const int X_pin = 0;  // analog pin connected to X output
-const int Y_pin = 1;  // analog pin connected to Y output
+const int X_pin = 0; // analog pin connected to X output
+const int Y_pin = 1; // analog pin connected to Y output
 const int Slider_pin = 2;
 
 RF24 radio(7, 8); // CE, CSN
@@ -33,23 +33,25 @@ void loop()
   int xMapped = map(xValue, 0, 1023, 0, 180);
   int yMapped = map(yValue, 0, 1023, 0, 180);
 
-    if(87<=xMapped || xMapped<=93)
-    {
-      xMapped = 90;
-    }
-    
-    if(87<=yMapped || yMapped<=93)
-    {
-      yMapped = 90;
-    }
+  if (87 <= xMapped && xMapped <= 93)
+  {
+    xMapped = 90;
+  }
+
+  if (87 <= yMapped && yMapped <= 93)
+  {
+    yMapped = 90;
+  }
 
   Serial.print("X-axis: ");
   Serial.println(xMapped);
   Serial.print("Y-axis: ");
   Serial.println(yMapped);
+  Serial.print("sliderValue: ");
+  Serial.println(sliderValue);
   Serial.print("\n\n");
 
-  if(xMapped_old != xMapped || yMapped_old != yMapped || sliderRaw_old != sliderValue)
+  if (xMapped_old != xMapped || yMapped_old != yMapped || sliderRaw_old != sliderValue)
   {
     const int text[3] = {xMapped, yMapped, sliderValue};
     radio.write(&text, sizeof(text));
